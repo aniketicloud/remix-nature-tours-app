97e66bc47b2a593f4c530e7d506ffd60e62943fe
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-01-09 00:22:27 +0530

    WIP: animated button

--- app/components/navigation/MainHeader.tsx
+++ app/components/navigation/MainHeader.tsx
@@ -4,12 +4,16 @@ export default function MainHeader() {
       <div className="logo-box">
         <img src="img/logo-white.webp" alt="logo" className="logo" />
       </div>
-      
+
       <div className="text-box">
         <h1 className="heading-primary">
           <span className="heading-primary-main">Outdoors</span>
           <span className="heading-primary-sub">is where life happens</span>
         </h1>
+
+        <a href="#discover" className="btn btn-white">
+          discover more
+        </a>
       </div>
     </header>
   );
